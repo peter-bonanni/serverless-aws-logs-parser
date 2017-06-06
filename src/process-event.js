75ca3b7689d5d3838e580a6e0c8acd5750565ced
Peter Bonanni <pbonanni@muxy.io>
2017-06-06 15:47:19 -0500

    Remove failing firhose test due to missing file. Add IP_address parsing and tests

--- src/process-event.js
+++ src/process-event.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const assert = require('assert')
-const moment = require('moment')
+const assert = require("assert");
+const moment = require("moment");
 
 const service_regex = {
-  'api-gateway': {
+  "api-gateway": {
     ts_start: /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z){1}/,
     ts_end: /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z){1} Method completed/,
     method: /HTTP Method: (.*?),/,
@@ -12,34 +12,34 @@ const service_regex = {
     request_query_string: /request query string: {(.*)}/,
     request_path: /request path: {(.*)}/,
     resource_path: /Resource Path: (.*)/,
-    response_status: /completed with status: (\d{3})/
+    response_status: /completed with status: (\d{3})/,
+    ip_address: /X-Forwarded-For=(\d+\.\d+\.\d+\.\d+)/
   },
-  'lambda': {
+  lambda: {
     ts_start: /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z){1} START/,
     ts_end: /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z){1} END/,
     request_id: /START RequestId: (.{36})/,
     duration_ms: /REPORT RequestId: .{36}	Duration: (\d+\.\d+)/,
     billed_duration_ms: /Billed Duration: (\d+) ms/,
     max_memory_used: /Max Memory Used: (\d+) MB/, //,
-    memory_size: /Memory Size: (\d+) MB/,
+    memory_size: /Memory Size: (\d+) MB/
   }
-}
+};
 
 module.exports = (service, txt) => {
-  let res = {}
-  let regex = service_regex[service]
-  assert(regex, `No such service: ${service}`)
+  let res = {};
+  let regex = service_regex[service];
+  assert(regex, `No such service: ${service}`);
   for (let expr of Object.keys(regex)) {
-    let match = regex[expr].exec(txt)
-    if(match && match.length > 1) {
-      res[expr] = match[1]
+    let match = regex[expr].exec(txt);
+    if (match && match.length > 1) {
+      res[expr] = match[1];
     } else {
-      res[expr] = 'N/A'
+      res[expr] = "N/A";
     }
-
   }
-  if (! res.duration_ms && res.ts_start && res.ts_end) {
-    res.duration_ms = moment(res.ts_end) - moment(res.ts_start)
+  if (!res.duration_ms && res.ts_start && res.ts_end) {
+    res.duration_ms = moment(res.ts_end) - moment(res.ts_start);
   }
-  return Object.assign(res, {raw: txt})
-}
+  return Object.assign(res, { raw: txt });
+};
--- tests/mocks/firehose-output.mock.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const contents = fs.readFileSync(`${__dirname}/firehose-sample.log`).toString();
-
-module.exports = contents
